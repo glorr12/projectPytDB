fff9e90b916e7892ebf4bb1b63dae21073d6debb
glorr12 <igor.fishkov1991@gmail.com>
2026-06-05 02:43:20 +0100

    mysql connection logic

--- database/sql_client.py
+++ database/sql_client.py
@@ -8,23 +8,31 @@ config = {
     'host': os.getenv('SQL_HOST'),
     'user': os.getenv('SQL_USER'),
     'password': os.getenv('SQL_PASSWORD'),
-    'db_name': os.getenv('DB_NAME'),
+    'db': os.getenv('DB_NAME'),
     'cursorclass': pymysql.cursors.DictCursor
 }
 
 
 class MySQLConnection:
+    """    Открывает новое соединение и курсор `DictCursor` в `__enter__`,
+    а затем закрывает оба в `__exit__`  независимо от того,
+    возникло исключение или нет"""
+
     def __init__(self, db_config: dict):
         self.__config = db_config
         self.__conn = None
         self.__cursor = None
 
     def __enter__(self):
+        """Установливает соединение с базой данных и открывает `DictCursor`"""
+
         self.__conn = pymysql.connect(**self.__config)
-        self.__cursor = self.__conn.cursor()
+        self.__cursor = self.__conn.cursor(pymysql.cursors.DictCursor)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """Закрывает курсор и соединение"""
+
         if self.__cursor:
             self.__cursor.close()
         if self.__conn:
@@ -32,16 +40,19 @@ class MySQLConnection:
         return False
 
 
-    def _validator(self,params):
+    @staticmethod
+    def _validator(params):
+        """Проверяет аргумент *params* перед передачей в курсор"""
+
         if params is None:
             return None
-        if not isinstance(params,(tuple,list,dict)):
+        if not isinstance(params, (tuple, list, dict)):
             raise TypeError("неверный тип данных")
         return params
 
     def execute(self, sql: str, params: tuple = None) -> list:
-        self._validator(params)
+        """Выполняет SQL-запрос и возвращает все строки результата"""
+
+        params = self._validator(params)
         self.__cursor.execute(sql, params)
         return self.__cursor.fetchall()
-
-
